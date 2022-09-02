b096f4c5e0f151aa4529c20938f50e402e3ddd09
Lin Amber <iltep118666@gmail.com>
2022-09-02 20:43:48 +0300

    k

---
+++ main.py
@@ -0,0 +1,16 @@
+from pprint import pprint
+import requests
+
+heroes = [332, 655, 659]
+stats = []
+
+def inteligens_request():                                                                     #достаём данные с сайта
+    for id in heroes:
+       url = f'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/powerstats/{id}.json'
+       response = requests.get(url)
+       stats.append(response.text)
+    pprint(stats)
+
+
+if __name__ == '__main__':
+    inteligens_request()
